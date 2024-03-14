c6dc1813f34f6d829504c1fde703f4f15facf936
lake <102535481@qq.com>
2024-03-14 16:35:59 +0800

    优化

--- src/views/pay/Mobile.vue
+++ src/views/pay/Mobile.vue
@@ -1,84 +1,49 @@
 <script setup>
+import {ref} from "vue";
+
+const menus = ref([
+  {
+    money: '8',
+    coin: '8',
+  }, {
+    money: '30',
+    origin: '32',
+    discount: '优惠',
+    coin: '32',
+  }, {
+    money: '68',
+    origin: '75',
+    coin: '75',
+    discount: '超值'
+  }
+])
+//优惠英文
 </script>
 
 <template>
   <div>
-    <div class="flex flex-col space-y-4">
-      <div class="dark-bg flex flex-row justify-between items-center shadow flex-1 rounded-2xl p-4">
-        <div class="flex flex-row items-center">
-          <div class="px-4">
-            <svg class="w-6 h-6 fill-accent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path
-                  d="M160 112v48H288V112c0-35.3-28.7-64-64-64s-64 28.7-64 64zm-48 96H48V416c0 26.5 21.5 48 48 48H352c26.5 0 48-21.5 48-48V208H336v56c0 13.3-10.7 24-24 24s-24-10.7-24-24V208H160v56c0 13.3-10.7 24-24 24s-24-10.7-24-24V208zm0-48V112C112 50.1 162.1 0 224 0s112 50.1 112 112v48h64c26.5 0 48 21.5 48 48V416c0 53-43 96-96 96H96c-53 0-96-43-96-96V208c0-26.5 21.5-48 48-48h64z"/>
-            </svg>
-          </div>
-          <div class="flex flex-col space-y-1">
-            <h1 class="text-lg space-x-1">
-              <span class="font-bold text-base-content/80">金币 8.00</span>
-              <span class="text-base-content/30">/</span>
-              <span class="text-sm font-bold text-base-content/50">￥8.00</span>
-            </h1>
-            <div>
-                <span class="text-xs font-bold rounded-full text-primary bg-sky-100 py-1 px-2">
-                  折扣 0%
-                </span>
+    <div class="flex flex-col divide-y">
+      <div class="flex flex-row py-4" v-for="item in menus">
+        <div class="flex flex-1 flex-row items-center space-x-2">
+          <div class="flex items-center space-x-1 text-lg">
+            <div class="font-medium space-x-1">
+              <span class="font-bold">{{ item.coin }}</span>
+              <span>金币</span>
             </div>
+            <div class="text-base-content/30">/</div>
+            <div class="text-base-content/50">
+              <span>￥</span>
+              <span>{{ item.money }}</span>
+            </div>
+          </div>
+          <div v-if="item.origin" class="line-through text-base-content/30">{{ item.origin }}</div>
+          <div v-if="item.discount"
+               class="text-sm items-center justify-center text-error border rounded px-1">
+            {{ item.discount }}
           </div>
         </div>
         <div>
-          <button class="btn btn-primary">购买</button>
-        </div>
-      </div>
-
-      <div class="dark-bg flex flex-row justify-between items-center shadow flex-1 rounded-2xl p-4">
-        <div class="flex flex-row items-center">
-          <div class="px-4">
-            <svg class="w-6 h-6 fill-accent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path
-                  d="M160 112v48H288V112c0-35.3-28.7-64-64-64s-64 28.7-64 64zm-48 96H48V416c0 26.5 21.5 48 48 48H352c26.5 0 48-21.5 48-48V208H336v56c0 13.3-10.7 24-24 24s-24-10.7-24-24V208H160v56c0 13.3-10.7 24-24 24s-24-10.7-24-24V208zm0-48V112C112 50.1 162.1 0 224 0s112 50.1 112 112v48h64c26.5 0 48 21.5 48 48V416c0 53-43 96-96 96H96c-53 0-96-43-96-96V208c0-26.5 21.5-48 48-48h64z"/>
-            </svg>
-          </div>
-          <div class="flex flex-col space-y-1">
-            <h1 class="text-lg space-x-1">
-              <span class="font-bold text-[#F19D38]">金币 32.00</span>
-              <span class="text-base-content/30">/</span>
-              <span class="text-sm font-bold text-base-content/50">￥30.00</span>
-            </h1>
-            <div>
-              <span class="text-xs font-bold rounded-full text-green-600 bg-yellow-100 py-1 px-2">折扣 93%</span>
-            </div>
-          </div>
-        </div>
-        <div>
-          <button class="btn btn-ghost btn-primary bg-[#F19D38] text-base-100">购买</button>
-        </div>
-      </div>
-
-      <div class="dark-bg flex flex-row justify-between items-center shadow flex-1 rounded-2xl p-4">
-        <div class="flex flex-row items-center">
-          <div class="px-4">
-            <svg class="w-6 h-6 fill-accent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path
-                  d="M160 112v48H288V112c0-35.3-28.7-64-64-64s-64 28.7-64 64zm-48 96H48V416c0 26.5 21.5 48 48 48H352c26.5 0 48-21.5 48-48V208H336v56c0 13.3-10.7 24-24 24s-24-10.7-24-24V208H160v56c0 13.3-10.7 24-24 24s-24-10.7-24-24V208zm0-48V112C112 50.1 162.1 0 224 0s112 50.1 112 112v48h64c26.5 0 48 21.5 48 48V416c0 53-43 96-96 96H96c-53 0-96-43-96-96V208c0-26.5 21.5-48 48-48h64z"/>
-            </svg>
-          </div>
-          <div class="flex flex-col space-y-1">
-            <h1 class="text-lg space-x-1">
-              <span class="font-bold text-base-content/80">金币 75.00</span>
-              <span class="text-base-content/30">/</span>
-              <span class="text-sm font-bold text-base-content/50">￥68.00</span>
-            </h1>
-            <div>
-                <span class="text-xs font-bold rounded-full text-primary bg-sky-100 py-1 px-2">
-                  折扣 90%
-                </span>
-            </div>
-          </div>
-        </div>
-        <div>
-          <button class="btn btn-primary">
-            购买
-          </button>
+          <button class="btn bg-yellow-400 text-base-100">购买</button>
         </div>
       </div>
     </div>
