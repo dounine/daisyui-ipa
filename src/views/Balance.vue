af8a5df4fb94d533575484edd8a4289622a40980
lake <102535481@qq.com>
2024-03-18 16:37:14 +0800

    优化

--- src/views/Balance.vue
+++ src/views/Balance.vue
@@ -1,50 +1,120 @@
 <script setup>
 import {ref} from 'vue'
-
+const data = ref({
+  coin: '8.00',
+})
+const result = ref([
+  {
+    type: '下载',
+    name: '微信',
+    secondary: '版本1.0.0',
+    coin: '-0.81',
+    time: '2021-09-01 12:00:00',
+  }, {
+    type: '提取',
+    name: '微信',
+    secondary: '版本1.0.0',
+    coin: '-0.01',
+    time: '2021-09-01 11:00:00',
+  }, {
+    type: '收到',
+    name: '用户1',
+    secondary: '金币',
+    coin: '+1.00',
+    time: '2021-09-01 10:02:00',
+  }, {
+    type: '赠送',
+    name: '用户123',
+    secondary: '金币',
+    coin: '-1.00',
+    time: '2021-09-01 10:01:00',
+  }, {
+    type: '充值',
+    name: '金额8.00',
+    secondary: '',
+    coin: '+8.00',
+    time: '2021-09-01 10:00:00',
+  }
+])
 </script>
 
 <template>
-  <div class="flex flex-row px-4">
-    <div class="grid grid-cols-2">
-      <div class="flex flex-1 relative">
-        <div class="absolute right-3 bg-[#5188EB] w-4 h-40 top-6 rounded-r-full -z-10"></div>
-        <div class="absolute right-1 bg-[#E0738E] w-4 h-32 top-10 rounded-r-full -z-20"></div>
-        <div
-            class="blance-card w-screen max-w-screen-sm h-52 flex flex-row rounded-xl p-6 mr-6">
-          <div class="flex flex-1 flex-col">
-            <div class="text-sm text-accent">13:00 / 2016-08-12</div>
-            <div class="font-medium text-white mt-1">Coin account 金币帐号</div>
-            <div class="flex flex-row items-center space-x-2 font-mono mt-8 text-xl text-white">
-              <div>0000</div>
-              <div>0001</div>
+  <div>
+    <div class="px-4">
+      <div class="grid grid-cols-2 mt-8">
+        <div class="flex flex-1 relative">
+          <div class="absolute right-3 bg-[#5188EB] w-4 h-40 top-6 rounded-r-full -z-10"></div>
+          <div class="absolute right-1 bg-[#E0738E] w-4 h-32 top-10 rounded-r-full -z-20"></div>
+          <div
+              class="blance-card w-screen max-w-screen-sm h-52 flex flex-row rounded-xl p-6 mr-6">
+            <div class="flex flex-1 flex-col">
+              <div class="text-sm text-accent">13:00 / 2016-08-12</div>
+              <div class="font-medium text-white mt-1">Coin account 金币帐号</div>
+              <div class="flex flex-row items-center space-x-2 font-mono mt-8 text-xl text-white">
+                <div>0000</div>
+                <div>0001</div>
+              </div>
             </div>
-          </div>
-          <div class="flex flex-col">
-            <div class="flex flex-1 justify-center">
-              <svg class="w-10 h-10 fill-accent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                <path
-                    d="M320 160A80 80 0 1 0 320 0a80 80 0 1 0 0 160zM208 328a80 80 0 1 0 0-160 80 80 0 1 0 0 160zM192 432A80 80 0 1 0 32 432a80 80 0 1 0 160 0zm128 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm288-80a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM432 328a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/>
-              </svg>
-            </div>
-            <div class="flex flex-col items-center">
-              <div class="text-accent text-sm">金币余额</div>
-              <div class="font-mono text-white font-bold">￥8.98</div>
+            <div class="flex flex-col">
+              <div class="flex flex-1 justify-center">
+                <svg class="w-10 h-10 fill-accent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                  <path
+                      d="M320 160A80 80 0 1 0 320 0a80 80 0 1 0 0 160zM208 328a80 80 0 1 0 0-160 80 80 0 1 0 0 160zM192 432A80 80 0 1 0 32 432a80 80 0 1 0 160 0zm128 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm288-80a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM432 328a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/>
+                </svg>
+              </div>
+              <div class="flex flex-col items-center">
+                <div class="text-accent text-sm">金币余额</div>
+                <div class="font-mono text-white font-bold">￥8.98</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="bg-info/10 rounded-xl p-4">
-        <div class="font-medium">
-          帐户操作
-        </div>
-        <div>
-          <button class="btn btn-sm btn-primary">充值</button>
-          <button class="btn btn-sm">转帐</button>
+        <div class="bg-info/10 rounded-xl p-4">
+          <div class="font-medium">
+            帐户操作
+          </div>
+          <div>
+            <button class="btn btn-sm btn-primary">充值</button>
+            <button class="btn btn-sm">转帐</button>
+          </div>
         </div>
       </div>
     </div>
     <div>
-
+      <div class="flex flex-row justify-between mt-8 px-4">
+        <div></div>
+        <div>
+          <button class="btn btn-outline btn-ghost rounded-full border-base-content/30 font-normal">
+            <i class="icon icon-filter"></i>
+            <span>搜索</span>
+          </button>
+        </div>
+      </div>
+      <div class="grid gap-4 px-4 mt-4">
+        <div v-for="item in result" class="dark-bg flex flex-row justify-between shadow px-4 py-2 rounded">
+          <div class="flex flex-row space-x-2">
+            <div class="flex items-center">
+              <i class="icon icon-download"></i>
+            </div>
+            <div>
+              <div class="flex flex-row space-x-1">
+                <div>{{ item.type }}</div>
+                <div class="font-bold">{{ item.name }}</div>
+                <div>{{ item.secondary }}</div>
+              </div>
+              <div class="text-base-content/50">{{ item.coin }}</div>
+            </div>
+          </div>
+          <div class="flex items-center">
+            <button class="btn rounded-full btn-sm font-medium">
+              查看
+            </button>
+          </div>
+        </div>
+        <div class="flex flex-row justify-center">
+          <button class="btn rounded-full font-medium">加载更多</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
