3adda68f74d6d48d39df528c8af6a9d2e2416a61
lake <102535481@qq.com>
2024-03-20 18:19:50 +0800

    优化

--- src/views/Dump.vue
+++ src/views/Dump.vue
@@ -52,8 +52,8 @@ onBeforeMount(async () => {
       <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-8">
         <div v-for="app in list"
              class="transition-transform duration-300 md:hover:-translate-y-px md:hover:translate-x-px">
-          <div class="item">
-            <div class="info">
+          <div class="dump-item">
+            <div class="dump-info">
               <div class="img-box">
                 <div class="loading-box">
                   <span class="loading loading-spinner loading-sm text-base-content/30"></span>
@@ -77,10 +77,35 @@ onBeforeMount(async () => {
                 </div>
               </div>
             </div>
-            <div class="download-box">
-              <RouterLink to="1/version" class="download-link">
-                <i class="icon icon-download download-icon"></i>
-              </RouterLink>
+            <progress class="progress progress-primary h-1 m-0 p-0" value="20" max="100"></progress>
+            <div class="flex flex-row justify-between py-2">
+              <div class="text-sm space-x-1 flex flex-row items-center">
+                <span class="font-medium font-mono">No.1</span>
+                <span class="text-base-content/30">/</span>
+                <span>排队中</span>
+              </div>
+              <div class="avatar-group -space-x-4">
+                <div class="avatar">
+                  <div class="w-8">
+                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                  </div>
+                </div>
+                <div class="avatar">
+                  <div class="w-8">
+                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                  </div>
+                </div>
+                <div class="avatar">
+                  <div class="w-8">
+                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                  </div>
+                </div>
+                <div class="avatar">
+                  <div class="w-8">
+                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -91,9 +116,9 @@ onBeforeMount(async () => {
 </template>
 
 <style lang="scss" scoped>
-.item {
-  @apply dark-bg flex flex-row opacity-95 shadow transition-all duration-300 md:hover:shadow-indigo-500/50 rounded-md px-4;
-  .info {
+.dump-item {
+  @apply dark-bg flex flex-col opacity-95 shadow transition-all duration-300 md:hover:shadow-indigo-500/50 rounded-md px-4;
+  .dump-info {
     @apply flex flex-row flex-1 my-4 space-x-2 md:truncate;
     .img-box {
       @apply flex items-center min-w-14 max-w-14 relative;
